d8f973ad26c5d80256f6acbaa3db0ab50a13a7c5
hannan <ansariabdulhannan321@gmail.com>
2025-07-18 17:19:31 +0530

    updated node version

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const taskRoutes = require('../routes/taskRoutes'); // Adjust the path if needed
+const taskRoutes = require('../routes/taskRoutes'); 
 
 dotenv.config();
 
