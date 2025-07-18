bd989221ef724614422279f0714c511d5ae6ad3b
hannan <ansariabdulhannan321@gmail.com>
2025-07-18 18:17:49 +0530

    updated index.js file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
+// backend/api/index.js
+
 const serverless = require('serverless-http');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const taskRoutes = require('../routes/taskRoutes'); // routing file for tasks
+const taskRoutes = require('../routes/taskRoutes'); 
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -21,4 +24,5 @@ mongoose
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB error:', err));
 
-module.exports = serverless(app);
+module.exports = app;
+module.exports.handler = serverless(app);
